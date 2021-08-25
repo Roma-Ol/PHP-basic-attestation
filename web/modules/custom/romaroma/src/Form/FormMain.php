a5317ce8d9a44e84dd5a2e0a35fb0b6645ed0b6f
RomaRoma49 <v12nes11@gmail.com>
2021-08-25 12:10:23 +0300

    hotfix

--- web/modules/custom/romaroma/src/Form/FormMain.php
+++ web/modules/custom/romaroma/src/Form/FormMain.php
@@ -23,26 +23,26 @@ class FormMain extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     // Current year.
     $year = date('Y');
-    // Counters for forms and lines.
+    // Counters for forms and rows.
     $number_of_forms = $form_state->get('number_of_forms');
     $number_of_rows  = $form_state->get('number_of_rows');
-
     if ($number_of_forms === NULL) {
       $number_of_forms = $form_state->set('number_of_forms', 1);
       $number_of_forms = 1;
     }
-
     if ($number_of_rows === NULL) {
-      $number_of_rows = $form_state->set('number_of_rows', 1);
-      $number_of_rows = 1;
+      $number_of_rows = $form_state->set('number_of_rows', 0);
+      $number_of_rows = 0;
     }
 
+    // Building a form.
+    // Creating a wrapper.
     $form['table'] = [
-      '#type' => 'fieldset',
+      '#type'   => 'fieldset',
       '#prefix' => '<div id="veritas-id-wrapper">',
       '#suffix' => '</div>',
     ];
-
+    // Form generator.
     for ($tables = 0; $tables < $number_of_forms; $tables++) {
       $form['#tree']          = TRUE;
       $form['table'][$tables] = [
@@ -70,13 +70,14 @@ class FormMain extends FormBase {
         '#prefix' => '<div id="veritas-id-wrapper">',
         '#suffix' => '</div>',
       ];
-
+      // Rows generator.
       for ($rows = 0; $rows < $number_of_rows; $rows++) {
         $form['table'][$tables][$rows]['year'] = [
           '#type'     => 'number',
           '#value'    => $year - $rows,
           '#disabled' => TRUE,
         ];
+        // Cells generator.
         for ($cells = 1; $cells <= 17; $cells++) {
           $form['table'][$tables][$rows][$cells] = [
             '#type' => 'number',
@@ -85,28 +86,29 @@ class FormMain extends FormBase {
       }
     }
 
-    // Add a row button.
+    // Buttons.
+    // 'Add a row' button.
     $form['actions']['add_row'] = [
       '#type'       => 'submit',
       '#value'      => t('+ Row'),
       '#submit'     => ['::addOneRow'],
       '#attributes' => ['class' => ['btn-transparent']],
       '#ajax'       => [
-        'callback' => '::addRowAJAX',
+        'callback' => '::addRowAjax',
         'wrapper'  => 'veritas-id-wrapper',
       ],
     ];
-    // Add form button.
+    // 'Add a form' button.
     $form['actions']['add_form'] = [
       '#type'   => 'submit',
       '#value'  => t('+ Form'),
       '#submit' => ['::addOneForm'],
       '#ajax'   => [
-        'callback' => '::addFormAJAX',
+        'callback' => '::addFormAjax',
         'wrapper'  => 'veritas-id-wrapper',
       ],
     ];
-    // Add a submit button that handles the submission of the form.
+    // 'Submit' button.
     $form['actions']['submit'] = [
       '#type'  => 'submit',
       '#value' => $this->t('Send'),
@@ -117,46 +119,51 @@ class FormMain extends FormBase {
 
   /**
    * {@inheritdoc}
+   *
+   * Basic form validation.
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     // Do validation.
   }
 
   /**
-   * AJAX adding a field.
+   * AJAX adding a row.
    */
-  public function addRowAJAX(array &$form, FormStateInterface $form_state) {
-    $form = $form_state->getCompleteForm();
-    return $form['table'];
-  }
-
-  public function addFormAJAX(array &$form, FormStateInterface $form_state) {
+  public function addRowAjax(array &$form, FormStateInterface $form_state) {
     $form = $form_state->getCompleteForm();
     return $form['table'];
   }
 
   /**
-   * Increment number of rows.
+   * AJAX adding a form.
+   */
+  public function addFormAjax(array &$form, FormStateInterface $form_state) {
+    $form = $form_state->getCompleteForm();
+    return $form['table'];
+  }
+
+  /**
+   * Increment the number of rows.
    */
   public function addOneRow(array &$form, FormStateInterface $form_state) {
     $number_of_rows = $form_state->get('number_of_rows');
-    $add_button1 = $number_of_rows +1;
+    $add_button1    = $number_of_rows + 1;
     $form_state->set('number_of_rows', $add_button1);
     $form_state->setRebuild();
   }
 
   /**
-   * Increment number of tables.
+   * Increment the number of forms.
    */
   public function addOneForm(array &$form, FormStateInterface $form_state) {
     $number_of_forms = $form_state->get('number_of_forms');
-    $add_button2 = $number_of_forms +1;
+    $add_button2     = $number_of_forms + 1;
     $form_state->set('number_of_forms', $add_button2);
     $form_state->setRebuild();
   }
 
   /**
-   * {@inheritdoc}
+   * Submit event.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->messenger()->addStatus($this->t('The message has been sent.'));
